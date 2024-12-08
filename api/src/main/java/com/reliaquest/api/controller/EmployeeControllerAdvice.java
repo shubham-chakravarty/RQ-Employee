806ea198bc31c09bc8b90d231766334c7e061bbf
shubham-chakravarty <chakravartys177@gmail.com>
2024-12-08 20:15:20 +0530

    added exception handler for Validation of Create API

--- api/src/main/java/com/reliaquest/api/controller/EmployeeControllerAdvice.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeControllerAdvice.java
@@ -4,8 +4,13 @@ import com.reliaquest.api.exception.EmployeeNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -21,6 +26,33 @@ public class EmployeeControllerAdvice {
     public ResponseEntity<String> handleEmployeeNotFoundException(EmployeeNotFoundException ex) {
         log.error("Error handling Employee Not Found.", ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    /**
+     * Handles validation exceptions triggered by {@code @Valid} annotation during request body validation.
+     * <p>
+     * This method is invoked whenever a {@link MethodArgumentNotValidException} is thrown in the application,
+     * typically due to validation errors in the incoming request body.
+     * It formats the validation errors into a user-friendly response structure and returns an HTTP status of 400 (Bad Request).
+     * </p>
+     *
+     * @param ex the exception that encapsulates the validation errors
+     * @return a {@link ResponseEntity} containing the error message and a list of detailed validation error descriptions.
+     *         Each error description specifies the invalid field and the corresponding error message.
+     *
+     * @see MethodArgumentNotValidException
+     * @see ResponseEntity
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", "Validation failed");
+        errorResponse.put("details", ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError -> String.format("Field '%s' %s", fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList()));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     /**
